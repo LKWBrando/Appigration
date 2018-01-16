f4394ba271e11a598997c5db316c9d9a24c44192
Constant-HughesTreherne <constant.hughestreherne@my.jcu,edu.au>
2018-01-16 09:29:52 +0800

    Added array for location entry in Buy Activity

--- app/src/main/java/example/assignmentone/cp3406/app_igration/BuyActivity.java
+++ app/src/main/java/example/assignmentone/cp3406/app_igration/BuyActivity.java
@@ -1,12 +1,17 @@
 package example.assignmentone.cp3406.app_igration;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.hardware.SensorEventListener;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.SeekBar;
 import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class BuyActivity extends AppCompatActivity {
 
@@ -14,12 +19,17 @@ public class BuyActivity extends AppCompatActivity {
     SeekBar seekBar;
     int value;
     TextView priceValue;
+    private EditText locationText;
+    Button okay;
 
-    public void onBrowsePressed(View view){
+    public void onBrowsePressed(View view) {
         Intent goToBrowse = new Intent(this, BrowseActivity.class);
         startActivity(goToBrowse);
 
     }
+
+    public static ArrayList<String> locationList = new ArrayList<String>();
+    //String locationList[];
 
 
     @Override
@@ -27,7 +37,9 @@ public class BuyActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_buy);
 
+        okay = (Button) findViewById(R.id.okay);
 
+        locationText = (EditText) findViewById(R.id.locationText);
 
         seekBar = (SeekBar) findViewById(R.id.seekBar);
 
@@ -38,6 +50,22 @@ public class BuyActivity extends AppCompatActivity {
         priceValue = (TextView) findViewById(R.id.price);
 
         final String price = Integer.toString(value);
+
+        okay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String location = locationText.getText().toString().trim();
+                if (location.length() !=0){
+                    locationList.add(location);
+                    locationText.setText("");
+                }
+            }
+        });
+
+
+
+
+
 
 
 
